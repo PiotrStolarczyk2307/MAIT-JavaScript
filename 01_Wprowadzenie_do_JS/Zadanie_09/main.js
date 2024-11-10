3ac666025259eb8c9c6ac5f76ccb646cef024b17
PiotrStolarczyk2307 <Piotrek23071999@gmail.com>
2024-11-10 10:58:04 +0100

    Day2-part2

--- 01_Wprowadzenie_do_JS/Zadanie_09/main.js
+++ 01_Wprowadzenie_do_JS/Zadanie_09/main.js
@@ -4,7 +4,7 @@ const currentDate = new Date();
 const currentYear = currentDate.getUTCFullYear();
 
 // Twój rok urodzenia (zmień na swój rok urodzenia)
-const birthYear = 2000; // <-- Zmień na swój rok urodzenia
+const birthYear = 1999; // <-- Zmień na swój rok urodzenia
 
 // Pętla, która wypisuje ciasto za każdy rok
 for (let year = birthYear; year <= currentYear; year++) {
